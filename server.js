66a211fbb21de6f228ce48a60cf611581b7afbb4
ahmell <andrewmell10@gmail.com>
2023-08-30 16:15:58 -0500

    added routes to API and HTML, and connected them to the server js file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
-const noteData = require('./db/db.json');
 
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
-
-app.get('/api', (req, res) => res.json(noteData));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes); //html routes need to be last to act as a catch-all
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
